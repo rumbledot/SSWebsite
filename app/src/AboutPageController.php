7bbea38b5467da67c8f3d2c228f8c60288b6ffca
Abe <bram.kurnanto@gmail.com>
2020-10-26 19:50:39 +1300

    removing SS auto append in AboutPage

--- app/src/AboutPageController.php
+++ app/src/AboutPageController.php
@@ -7,5 +7,9 @@ use SilverStripe\View\SSViewer;
 
 class AboutPageController extends PageController
 {
-
+    protected function init()
+    {
+        parent::init();
+        SSViewer::setRewriteHashLinksDefault(false);
+    }
 }
